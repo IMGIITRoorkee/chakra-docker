8228869c81b6c8528a199bcea9ded2f63accb8f8
Radhika <radhika@cs.iitr.ac.in>
2022-01-19 17:03:23 +0530

    Fix development script for docker on wsl2
    Co-authored-by: Rishi Ranjan <rish9101@users.noreply.github.com>

--- scripts/development.sh
+++ scripts/development.sh
@@ -2,7 +2,29 @@
 
 # Let's start the docker container for DB
 
-docker-compose up -d database
+docker network create -d bridge chakra-dev-network
+
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "Stopping the dev server"
+    docker stop 5432
+    docker network rm chakra-dev-network
+    exit
+}
+
+docker run \
+    --tty \
+    --rm \
+    -d \
+    --publish 5432:5432/tcp \
+    --user postgres \
+    --network chakra-dev-network \
+    --volume database:/var/lib/postgresql/data \
+    --name=5432 \
+    --network-alias postgres5432 \
+    --env-file postgres/database.env \
+    chakra-postgres:latest
 
 # Starting the flask server for chakra-core
 
@@ -10,3 +32,4 @@ source codebase/chakra-core/.env
 cd codebase/chakra-core
 source chakra-core-env/bin/activate
 flask run
+
