915160033d047d544c4f45f5b88ad52939ca7279
rish9101 <rranjan@cs.iitr.ac.in>
2022-02-01 05:13:35 -0500

    Add interrupt signal handler in prod script

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -4,6 +4,17 @@
 
 docker-compose up -d
 
+trap handle_interrupt INT
+
+function handle_interrupt() {
+	cd ../../
+	docker-compose down
+	exit 0
+}
+
 cd codebase/chakra-core
 source chakra-core-env/bin/activate
-flask run
+flask run --host=0.0.0.0
+
+cd ../../
+docker-compose down
